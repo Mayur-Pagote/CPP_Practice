2c9cfb83f24a8e268c4d41fc36d9729081b89f8e
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-08-07 15:18:50 +0530

    Create 2. Calculating SI.cpp

---
+++ Basic/2. Calculating SI.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float p,r,t,si;
+    p = 50;
+    r = 12;
+    t = 20;
+    si = (p*r*t)/100;
+    cout<<"Your Simple interest is: "<<si;
+}
