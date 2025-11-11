87c06f31b0627a65390318731c26adbe5491d47c
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-11-11 19:09:33 +0530

    Create Divisible by 5 or 3.cpp

---
+++ Module 04 - Conditions/Divisible by 5 or 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    cout<<endl;
+
+    if (num % 5 == 0 || num % 3 == 0){
+        cout<<"It is div by 5 or 3";
+    }
+    else {
+        cout<<"No";
+    }
+
+}
