cc9ae644c241d9783f6a8069aa9e3584d9c0f417
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-11-11 19:36:52 +0530

    Update Check alphabet or not.cpp

--- Module 04 - Conditions/Check alphabet or not.cpp
+++ Module 04 - Conditions/Check alphabet or not.cpp
@@ -7,7 +7,7 @@ int main() {
     cout<<"Enter a character: ";
     cin>>s;
 
-    if ((int)s>=65 && (int)s <= 122 ){
+    if (((int)s>=65 && (int)s <= 90) || ((int)s>=97 && (int)s <= 122) ){
         cout<<"Yes is it a alphabet";
     }
     else {
