3e41270cf9662f36e3f6f0528c972f1894aa09a0
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-11-13 22:06:38 +0530

    Create Factorials till n.cpp

---
+++ Module 05 - Loop/Factorials till n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, count = 1;
+    cout<<"Enter a number: ";
+    cin>>num;
+    
+    for (int i = 1; i<=num; i++){ 
+        count *= i;
+        cout<<"Factorial of "<<i<<" is "<<count<<endl;
+    }
+}
