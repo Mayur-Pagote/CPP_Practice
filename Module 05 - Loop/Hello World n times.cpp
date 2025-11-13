0332ed8d0d4f92961c2ba5485d46f409a01ff004
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-11-13 09:30:40 +0530

    Create Hello World n times.cpp

---
+++ Module 05 - Loop/Hello World n times.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter times: ";
+    cin>>num;
+
+    for (int i = 1; i <= num; i++){
+        cout<<"Hello World"<<endl;
+    }
+
+}
