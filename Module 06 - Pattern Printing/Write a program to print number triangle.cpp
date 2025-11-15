6f8c270e66df8f94a84bb2c8634465599a46c02d
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-11-15 09:44:19 +0530

    Create Write a program to print number triangle.cpp

---
+++ Module 06 - Pattern Printing/Write a program to print number triangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main() {
+    // Write a program to print number triangle.
+    int num;
+    cout<<"Enter number: ";
+    cin>>num;
+
+
+    for (int i = 1; i<= num; i++){
+        for (int j = 1; j <= i ;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+
+}
