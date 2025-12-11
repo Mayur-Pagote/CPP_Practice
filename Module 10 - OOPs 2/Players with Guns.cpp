9c5f248d4f75cf7d8a23112a1f50b01276e711ca
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-12-11 19:34:34 +0530

    Create Players with Guns.cpp

---
+++ Module 10 - OOPs 2/Players with Guns.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+
+class Gun {
+    public:
+    int ammo;
+    int damage;
+};
+
+class Player {
+private:
+    int health;
+    int age;
+    int score;
+    bool alive;
+    Gun gun;
+
+public:
+    int getHealth(){
+        return health;
+    }
+    int getAge(){
+        return age;
+    }
+    int getScore(){
+        return score;
+    }
+    bool isAlive(){
+        return alive;
+    }
+    Gun getGun(){
+        cout<<"Gun ammo is: "<<gun.ammo<<endl; 
+        cout<<"Gun damage is: "<<gun.damage<<endl;
+    }
+    void setHealth(int health){
+        this->health = health;
+    }
+    void setAge(int a){
+        age = a;
+    }
+    void setScore(int s){
+        score = s;
+    }
+    void setisAlive(bool al){
+        alive = al;
+    }
+    void setGun(Gun gun){
+        this->gun = gun;
+    }
+};
+
+
+
+int predictWin(Player a, Player b){
+    if (a.getHealth() > b.getHealth()) return a.getHealth();
+    else b.getHealth();
+}
+
+
+
+int main () {
+
+    Player cheese;
+    Player burger;
+
+    Gun akm;
+    akm.ammo = 100;
+    akm.damage = 20;
+
+    Gun awm;
+    awm.ammo = 10;
+    awm.damage = 120;
+
+    cheese.setAge(12);
+    cheese.setHealth(100);
+    cheese.setScore(20);
+    cheese.setisAlive(true);
+    cheese.setGun(akm);
+
+    burger.setAge(22);
+    burger.setHealth(90);
+    burger.setScore(200);
+    burger.setisAlive(true);
+    burger.setGun(awm);
+
+
+    cout<<predictWin(cheese, burger)<<endl;
+
+    burger.getGun();
+    
+}
