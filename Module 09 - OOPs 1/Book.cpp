d1c5fc8282ead31e7dbad5cba8e01fb49ef5b295
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-12-11 17:47:17 +0530

    Create Book.cpp

---
+++ Module 09 - OOPs 1/Book.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+class book {
+    public:
+    char name;
+    int price;
+    int pages;
+
+    int countBook(int p){
+        if (price<p) return 1;
+        else return 0;
+    }
+
+    bool isBookPresent(char book){
+        if (name == book) return true;
+        else return false; 
+    }
+};
+
+
+int main () {
+    book IKIGAI;
+    IKIGAI.name = 'I';
+    IKIGAI.price = 500;
+    IKIGAI.pages = 100;
+
+    cout<<IKIGAI.countBook(500)<<endl;
+    cout<<IKIGAI.isBookPresent('I')<<endl;
+}
