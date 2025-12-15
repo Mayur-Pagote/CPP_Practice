5b4cd417c72ef17b1aeecd9153f0ed14e27b04b9
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2025-12-15 18:35:18 +0530

    Create Constructor.cpp

---
+++ Module 11 - OOPs 3/Constructor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Bike {
+    public:
+    int wheels;
+
+    Bike () {
+        cout<<"This is wokring: ";
+    }
+
+};
+
+int main() {
+
+    Bike BMW;
+
+
+}
