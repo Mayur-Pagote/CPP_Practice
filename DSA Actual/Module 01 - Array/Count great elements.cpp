0581c9d0282b716a358202c24607f505ec18c088
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-01-02 17:02:25 +0530

    Create Count great elements.cpp

---
+++ DSA Actual/Module 01 - Array/Count great elements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main () {
+    int arr[10] = {3,54,2,4,6,34,23,35,56,34};
+
+    int count = 0, num = 20;
+
+    for (int i = 0; i<10 ;i++) {
+        if (arr[i] > num) count += 1;
+    }
+
+    cout<<count;
+}
