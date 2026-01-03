3d7554b57e55c157e351bf45b95e261243b39bf4
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-01-03 19:49:48 +0530

    Create Vector sort.cpp

---
+++ DSA Actual/Module 02 - Array and Vector/Vector sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main () {
+    vector<int> v;
+    
+    for (int  i = 0 ; i <= 5; i++){
+        int x ; 
+        cin>>x;
+        v.push_back(x);
+    }
+
+    for (int  i = 0 ; i <= 5; i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+    sort(v.begin(),v.end());
+
+    for (int  i = 0 ; i <= 5; i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
