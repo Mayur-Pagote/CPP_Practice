7a177345ade95bb54f68caf3ad75e4e421c6a3c1
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-01-06 21:28:49 +0530

    Create 3 color 3 pointer.cpp

---
+++ DSA Actual/Module 02 - Array and Vector/3 color 3 pointer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+    vector <int> v = {1,0,1,2,1,2,2,0,0,1,2,1,2,0};
+
+    int low = 0, mid = 0, high = v.size()-1;
+    while (mid <= high){
+        if (v[mid] == 2) {
+            swap(v[mid], v[high]);
+            high -= 1;
+        }
+        else if (v[mid] == 0){
+            swap(v[low], v[mid]);
+            low += 1;
+            mid += 1;
+        }
+        else if (v[mid] == 1){
+            mid += 1;
+        }
+    }
+
+    for (int i = 0 ; i < v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+}
