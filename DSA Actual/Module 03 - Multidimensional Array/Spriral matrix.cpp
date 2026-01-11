c1ef0331e76f27da9fa1517a190236e1b3e6cc9e
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-01-11 10:24:08 +0530

    Create Spriral matrix.cpp

---
+++ DSA Actual/Module 03 - Multidimensional Array/Spriral matrix.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row, col;
+
+    cout << "Enter rows of matrix one: ";
+    cin >> row;
+
+    cout << "Enter cols of matrix one: ";
+    cin >> col;
+
+    int mat1[row][col];
+    
+    cout << "Enter elements in matrix 1:\n";
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cin >> mat1[i][j];
+        }
+    }
+
+    for(int i = 0 ; i < row ; i++){
+        if (i % 2 == 0 ){
+            for(int j = 0; j < col; j++) cout<<mat1[i][j]<<" ";
+            
+        }
+        else {
+            for (int j = col-1; j >= 0; j--) cout<<mat1[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
