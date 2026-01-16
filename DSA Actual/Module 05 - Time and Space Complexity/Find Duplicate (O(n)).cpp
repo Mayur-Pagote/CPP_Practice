56ea86c07dd6904265366891851679079a45eca6
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-01-16 16:37:36 +0530

    Create Find Duplicate (O(n)).cpp

---
+++ DSA Actual/Module 05 - Time and Space Complexity/Find Duplicate (O(n)).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+// 6 1 2 4 3 2 7 5
+int main () {
+    int length;
+    cin>>length;
+    vector<int> arr;
+    vector<int> check(length+1,0);
+    for(int i = 1; i <= length; i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+    for(int j = 0; j < length; j++){
+        check[arr[j]] += 1;
+    }
+
+    for(int j = 1; j <= length; j++){
+        if (check[j]>1) cout<<j;
+    }
+
+
+    
+}
