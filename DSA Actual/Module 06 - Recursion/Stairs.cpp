8f8b0431c3df4372eaa0e6030cff447005b9242f
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-01-21 17:33:06 +0530

    Create Stairs.cpp

---
+++ DSA Actual/Module 06 - Recursion/Stairs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int stair(int num){
+    if (num == 1) return 1;
+    if (num == 2) return 2;
+    else return stair(num-1) + stair(num-2);
+}
+
+int main () {
+    
+    cout<<stair(3);
+}
