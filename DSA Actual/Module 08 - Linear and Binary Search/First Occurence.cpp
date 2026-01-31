a43858859f85d0343652efb99263840550ca258d
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-01-31 11:48:49 +0530

    Create First Occurence.cpp

---
+++ DSA Actual/Module 08 - Linear and Binary Search/First Occurence.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+    int size;
+    cin>>size;
+
+    vector<int> arr;
+    for(int i = 0; i< size; i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+
+    int target;
+    cin>>target;
+
+    int low = 0, high = size-1;
+    while(low <= high){
+        int mid = (low+high) / 2;
+        if (arr[mid] == target){
+            if (mid>0 &&arr[mid-1] == target){
+                high = mid-1;
+            }
+            else {
+                cout<<mid;
+                break;
+            }
+        }
+        if (arr[mid] > target) high = mid-1;
+        else  low = mid+1;
+    }
+}
