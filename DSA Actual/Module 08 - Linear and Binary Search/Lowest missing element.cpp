0f9ba727ef81fa4bcff73eefe7c039a96658c9d1
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-01-31 19:20:43 +0530

    Update Lowest missing element.cpp

--- DSA Actual/Module 08 - Linear and Binary Search/Lowest missing element.cpp
+++ DSA Actual/Module 08 - Linear and Binary Search/Lowest missing element.cpp
@@ -4,18 +4,15 @@ using namespace std;
 
 
 int main () {
-    vector<int> arr = {0,1,2,3,4,5,7};
-    int low = 0, high = arr.size()-1, ans = 0;
+    vector<int> arr = {1,2,3,4,5};
+    int low = 0, high = arr.size()-1, ans = -1;
     while (low <= high){
         int mid = (low+high) / 2;
         if (arr[mid] == mid){
-            if (mid+1 < arr.size() && arr[mid+1] == mid+1) low = mid + 1;
-            else {
-                ans = mid+1;
-                break;
-            }
+            low = mid + 1;
         }
-        else if (arr[mid] != mid){
+        else {
+            ans = mid;
             high = mid - 1;
         }
     }
