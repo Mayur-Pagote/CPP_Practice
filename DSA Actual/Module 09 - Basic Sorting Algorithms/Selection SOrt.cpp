67fe3f846cff1f6bdea66577f662b20702fd5f9a
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-02-08 09:56:20 +0530

    Create Selection SOrt.cpp

---
+++ DSA Actual/Module 09 - Basic Sorting Algorithms/Selection SOrt.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void displayVec(vector<int> arr){
+    for(int i =0 ; i < arr.size(); i++){
+        cout<<arr[i];
+    }
+}
+
+int main () {
+    vector<int> arr= {5,3,1,4,2};
+    int idx = 0;
+    while (true){
+        int minVal = arr[idx];
+        int minIdx = idx;
+        for (int i = idx; i < arr.size() ; i++){
+            if (minVal > arr[i]) {
+                minVal = arr[i];
+                minIdx = i;
+            }
+        }
+        swap(arr[idx], arr[minIdx]);
+        idx += 1;
+
+        if (idx >= arr.size()-1) break;
+    }
+    displayVec(arr);
+}
