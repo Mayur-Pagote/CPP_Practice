e3c01e826b5a8713f6a102322c0682fdfd5a9970
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-05-05 11:44:35 +0530

    Create Inversion Count Test.cpp

---
+++ DSA Actual/Module 10 - Advanced Sorting/Inversion Count Test.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int global_count = 0;
+
+int inversion (vector<int> &arr1, vector<int> &arr2){
+    int i = 0,  j = 0;
+    while (i < arr1.size() && j < arr2.size()){
+        if (arr1[i] > arr2[j]) {
+            global_count += arr1.size() - i;
+            j += 1;
+        }
+        else i+=1;
+    }
+    return global_count;
+}
+
+void merge(vector<int> &arr1, vector<int> &arr2, vector<int> &res) {
+    res.clear();
+    int i = 0, j = 0;
+
+    while (i < arr1.size() && j < arr2.size()) {
+        if (arr1[i] <= arr2[j]) {
+            res.push_back(arr1[i]);
+            i++;
+        } else {
+            res.push_back(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.size()) {
+        res.push_back(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.size()) {
+        res.push_back(arr2[j]);
+        j++;
+    }
+}
+
+void mergeSort(vector<int> &ans){
+    int n = ans.size();
+    if (n== 1)return;
+    int left = n/2, right = n - n/2;
+    vector<int> a(left);
+    vector<int> b(right);
+
+    for (int i = 0 ; i < left; i++){
+        a[i] = ans[i];
+    }
+
+    for (int k = 0; k < right;k++){
+        b[k] = ans[k+left];
+    }
+
+    mergeSort(a);
+    mergeSort(b);
+    inversion(a,b);
+    merge(a,b,ans);
+}
+
+
+
+int main () {
+
+    vector<int> res;
+
+    int length;
+    cin>>length;
+
+    for (int i = 0 ; i < length ; i++){
+        int temp;
+        cin>>temp;
+
+        res.push_back(temp);
+    }
+
+    mergeSort(res);
+    cout<<global_count;
+    // for (int i = 0 ; i < res.size(); i++){
+    //     cout<<res[i]<<" ";
+    // }
+
+
+
+}
