490633b4f13a4bad2351afe55ceecb47089f20e0
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-05-06 19:27:02 +0530

    Create Quick Sort Self Try.cpp

---
+++ DSA Actual/Module 10 - Advanced Sorting/Quick Sort Self Try.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition (vector<int> &arr, int si, int ei){
+    int startElement = arr[si];
+    int count = 0;
+
+    for (int i = si; i<=ei; i++){
+        if (startElement > arr[i]) count += 1;
+    }
+
+    int pivot = si+count;
+
+    swap (arr[si], arr[pivot]);
+    int i = si, j = ei;
+    while (i < pivot && j > pivot){
+        if (arr[i] < startElement) i++;
+        else if (arr[j] > startElement) j--;
+        else if (arr[i] > startElement && arr[j] < startElement){
+            swap(arr[i], arr[j]);
+            i += 1;
+            j -= 1;
+        } 
+    }
+    return pivot;
+}
+
+
+void quicksort(vector<int> &arr, int si, int ei){
+
+    if (si >= ei) return;
+
+    int pivotIdx = partition(arr, si,ei);
+
+    quicksort(arr, si, pivotIdx-1);
+    quicksort(arr, pivotIdx+1, ei);
+
+
+}
+
+int main () {
+    vector <int> arr = {7,4,6,9,6,4,2,1};
+    int length = arr.size();
+    quicksort(arr, 0, length-1);
+
+    for (int i = 0 ; i < length ; i++){
+        cout<<arr[i]<<" ";
+    }
+}
