f66a44190a95502a73b0eedf1ea492225f006f67
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-05-06 19:35:53 +0530

    Update Quick Sort Self Try.cpp

--- DSA Actual/Module 10 - Advanced Sorting/Quick Sort Self Try.cpp
+++ DSA Actual/Module 10 - Advanced Sorting/Quick Sort Self Try.cpp
@@ -6,8 +6,8 @@ int partition (vector<int> &arr, int si, int ei){
     int startElement = arr[si];
     int count = 0;
 
-    for (int i = si; i<=ei; i++){
-        if (startElement > arr[i]) count += 1;
+    for (int i = si+1; i<=ei; i++){
+        if (startElement >= arr[i]) count += 1;
     }
 
     int pivot = si+count;
@@ -15,13 +15,14 @@ int partition (vector<int> &arr, int si, int ei){
     swap (arr[si], arr[pivot]);
     int i = si, j = ei;
     while (i < pivot && j > pivot){
-        if (arr[i] < startElement) i++;
+        if (arr[i] <= startElement) i++;
         else if (arr[j] > startElement) j--;
         else if (arr[i] > startElement && arr[j] < startElement){
             swap(arr[i], arr[j]);
             i += 1;
             j -= 1;
         } 
+       
     }
     return pivot;
 }
@@ -40,7 +41,7 @@ void quicksort(vector<int> &arr, int si, int ei){
 }
 
 int main () {
-    vector <int> arr = {7,4,6,9,6,4,2,1};
+    vector <int> arr = {4,6,3,8,2,8,1};
     int length = arr.size();
     quicksort(arr, 0, length-1);
 
