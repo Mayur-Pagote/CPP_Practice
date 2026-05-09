58656309d403df0db84f4dc6291ff3c6a33b2406
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-05-09 08:31:28 +0530

    Update Running Sum of 1d Array.cpp

--- DSA Actual/Module 11 - Prefix Sum/Running Sum of 1d Array.cpp
+++ DSA Actual/Module 11 - Prefix Sum/Running Sum of 1d Array.cpp
@@ -1,10 +1,26 @@
-class Solution {
-public:
-    vector<int> runningSum(vector<int>& nums) {
-        int pre_sum = 0;
-        for (int i = 1; i<nums.size();i++){
-            nums[i] += nums[i-1];
-        }
-        return nums;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	// your code goes here
+    vector<int> arr;
+    int user;
+    cin>>user;
+    for (int i = 0 ; i < user; i++){
+        int temp ;
+        cin>>temp;
+        arr.push_back(temp);
     }
-};
+    
+    for (int j = 1 ; j < user;j++){
+        arr[j] = arr[j-1]+arr[j];
+    }
+
+    for (int j = 0 ; j < user;j++){
+        cout<<arr[j]<<" ";
+    }
+
+
+	return 0;
+}
