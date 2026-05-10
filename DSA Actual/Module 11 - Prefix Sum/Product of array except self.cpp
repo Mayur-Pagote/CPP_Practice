b989138ad13e7f75844ab7215c72ea2f40782061
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-05-10 08:37:42 +0530

    Create Product of array except self.cpp

---
+++ DSA Actual/Module 11 - Prefix Sum/Product of array except self.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+    vector<int> arr = {1,2,3,4};
+    int length = arr.size();
+    vector<int> prefix  (length , 1);
+    vector<int> suffix  (length , 1);
+
+    for (int i = 1 ; i < length ; i++){
+        prefix[i] *= arr[i-1] * prefix[i-1];
+        suffix[length-i-1] *= arr[length-i] * suffix[length-i];
+    }
+
+    for (int i = 0 ; i < length ; i++){
+        arr[i] = prefix[i] * suffix[i];
+    }
+
+    for (int i = 0 ; i < length ; i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+    
+}
