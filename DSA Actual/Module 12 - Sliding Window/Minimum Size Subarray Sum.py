7463540bdfc046729f0336361e9b09a13fb5ca37
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-05-21 20:59:06 +0530

    Update Minimum Size Subarray Sum.py

--- DSA Actual/Module 12 - Sliding Window/Minimum Size Subarray Sum.py
+++ DSA Actual/Module 12 - Sliding Window/Minimum Size Subarray Sum.py
@@ -1,23 +1,23 @@
-class Solution:
-    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        length = len(nums)
-        min_count = length + 1
-        i = 0
-        j = 0
+length = int(input())
+nums = list(map(int, input().split()))
+target = int(input())
+min_count = length + 1
+i = 0
+j = 0
+sum_var = 0
+while j < length and i < length:
+    sum_var += nums[j]
+    if sum_var >= target:
+        temp = j - i + 1
+        min_count = min(min_count, temp)
+        while sum_var >= target:
+            sum_var -= nums[i]
+            i += 1
+        temp = j - i + 2
+        min_count = min(min_count, temp)
+    j += 1
 
-        sum_var = 0
-        while (j<length and i < length):
-            sum_var += nums[j]
-            if sum_var >= target:
-                temp = j-i+1
-                min_count = min(min_count, temp)
-                while (sum_var>=target):
-                    sum_var -= nums[i]
-                    i+=1
-                temp = j-i+2
-                min_count = min(min_count, temp)
-                    
-            j += 1
-        if min_count == len(nums) +1:
-            return 0
-        return min_count
+if min_count == len(nums) + 1:
+    print(0) 
+else:
+    print(min_count)
