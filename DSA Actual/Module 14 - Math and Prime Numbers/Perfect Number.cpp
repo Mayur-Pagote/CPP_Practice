3566849aea73436a0d54483be8d6241a0e2a7b23
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-06-04 20:21:19 +0530

    Create Perfect Number.cpp

---
+++ DSA Actual/Module 14 - Math and Prime Numbers/Perfect Number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num, sum = 0;
+    cin>>num;
+    for (int i = 1; i< num;i++){
+        if (num%i == 0) sum += i;
+    }
+    if (sum == num) cout<<"true";
+    else cout<<"false";
+    return 0;
+}
