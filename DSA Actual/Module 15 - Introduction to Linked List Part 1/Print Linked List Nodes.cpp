c23d063fe4a4872cccc21e6e864fa2e9d955cbf5
Mayur Pagote <161322140+Mayur-Pagote@users.noreply.github.com>
2026-07-02 17:03:15 +0530

    Create Print Linked List Nodes.cpp

---
+++ DSA Actual/Module 15 - Introduction to Linked List Part 1/Print Linked List Nodes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace  std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node (int val){
+        this->val = val;
+    }
+
+};
+
+int main () {
+    Node a(10);
+    Node b(20);
+    Node c(30);
+
+    a.next = &b;
+    b.next = &c;
+    c.next = NULL;
+
+    cout<<((a.next)->next)->val;
+
+}
